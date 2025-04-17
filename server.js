19406e2da201be07fe557c9a517a4065b44663d6
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-04-17 19:35:43 +0530

    vercel json

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 const express = require("express");
 const app = express();
-// const userAuth = require("./middleware/userauth");
+const userAuth = require("./middleware/userauth");
 const conectDB = require("./config/databse");
-// const User = require("./models/userSchema");
-// const authRouter = require("./routes/authRouter");
-// const profileRouter = require("./routes/profileRouter");
-// const requestRouter = require("./routes/request");
-// const userRouter = require("./routes/user");
-// const cors = require("cors");
-// const cookieParser = require("cookie-parser");
-// const http = require("http");
-// const intializeSocket = require("./utils/socket");
-// const chatRouter = require("./routes/chatRouter");
+const User = require("./models/userSchema");
+const authRouter = require("./routes/authRouter");
+const profileRouter = require("./routes/profileRouter");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const intializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chatRouter");
 require("dotenv").config();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-// app.use(express.json()); // for reading json req data
-// authRouter.use(cookieParser());
+app.use(express.json()); // for reading json req data
+authRouter.use(cookieParser());
 
-// app.use("/", authRouter);
-// app.use("/", profileRouter);
-// app.use("/", requestRouter);
-// app.use("/", userRouter);
-// app.use("/", chatRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+app.use("/", chatRouter);
 app.use("/", (req, res) =>
   res.status(201).json({ message: "DevTinder Running" })
 );
-// app.use("/", (req, res, err) => {
-//   //
-//   if (err) {
-//     res.status(500).send("something went wrong");
-//   }
-// });
-// const server = http.createServer(app);
-// intializeSocket(server);
+app.use("/", (req, res, err) => {
+  //
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+const server = http.createServer(app);
+intializeSocket(server);
 
 conectDB()
   .then((result) => {
