51923750b43743b400067c1b02ecb865ac9dae8d
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-04-17 15:49:13 +0530

    new update

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const app = express();
-const userAuth = require("./middleware/userauth");
+// const userAuth = require("./middleware/userauth");
 const conectDB = require("./config/databse");
-const User = require("./models/userSchema");
-const authRouter = require("./routes/authRouter");
-const profileRouter = require("./routes/profileRouter");
-const requestRouter = require("./routes/request");
-const userRouter = require("./routes/user");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const http = require("http");
-const intializeSocket = require("./utils/socket");
-const chatRouter = require("./routes/chatRouter");
-require("dotenv").config();
+// const User = require("./models/userSchema");
+// const authRouter = require("./routes/authRouter");
+// const profileRouter = require("./routes/profileRouter");
+// const requestRouter = require("./routes/request");
+// const userRouter = require("./routes/user");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+// const http = require("http");
+// const intializeSocket = require("./utils/socket");
+// const chatRouter = require("./routes/chatRouter");
+// require("dotenv").config();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 // app.use(express.json()); // for reading json req data
 // authRouter.use(cookieParser());
@@ -44,7 +44,7 @@ app.use("/", (req, res) =>
 conectDB()
   .then((result) => {
     console.log("databse connected sucessfully");
-    server.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server Connected successfully");
     });
   })
